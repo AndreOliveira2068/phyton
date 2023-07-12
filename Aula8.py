134ee513a3af4452d600bdbd17f11dbe6e8c370f
ANDRE CRISPIM DE OLIVEIRA <105825998+AndreOliveira2068@users.noreply.github.com>
2023-07-12 19:23:16 -0300

    Add files via upload

---
+++ Aula8.py
@@ -0,0 +1,37 @@
+
+contador = 0
+
+while contador <= 100:
+    contador += 1
+    
+    if contador == 6:
+        print('não vou mostrar 0 6. ')
+        continue
+    if contador >= 10 and contador <= 27:
+        print ('não vou mostrar  o, contador')
+        continue
+    
+    print(contador)
+    
+    if contador == 40:
+        break
+    
+    
+    print('acabou')
+    
+    
+    qtd_Linhas =  4
+    qtd_Linhas =  4
+    
+    linha = 1
+    while linha <= qtd_Linhas:
+        
+        print(linha)
+        
+        linha += 1
+        
+        
+        print('acabou')
+    
+    
+        
---
+++ Aula9.py
@@ -0,0 +1,16 @@
+
+
+#.....012345678910111213
+nome = 'andre crispim'
+
+tamanho_nome = len (nome)
+
+indice = 0
+while indice < len(nome):
+    print(nome [indice])
+    indice += 1
+
+
+
+
+
